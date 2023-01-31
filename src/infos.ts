7d2df135a134ca58bbe688a4dec9a59c0fbd38c5
kity <geekcat@protonmail.com>
2023-01-31 23:21:22 +0100

    moved infos to a new folder

---
+++ src/infos.ts
@@ -0,0 +1,14 @@
+export const siteName = "harryssecrets";
+
+export const socialSites = [
+  {
+    site: "GitHub",
+    username: "harryssecret",
+    url: "https://github.com/harryssecret",
+  },
+  {
+    site: "Mastodon",
+    username: "bleeehpussycat",
+    url: "https://noc.social/@blehhpussycat",
+  },
+];
